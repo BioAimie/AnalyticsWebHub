2977cae1f3bccf731a7a52988556fc012398e9b8
bioamber <amber.kiser@biofiredx.com>
2017-01-19 09:15:22 -0700

    v2.8.0
    Fixed CI query that was broken by trackers DB update.

--- SQL/R_CI_EscalatedComplaintsPerComplaint.sql
+++ SQL/R_CI_EscalatedComplaintsPerComplaint.sql
@@ -1,7 +1,7 @@
-SELECT
+SELECT 
 	YEAR([CreatedDate]) AS [Year],
 	DATEPART(ww,[CreatedDate]) AS [Week],
-	IIF([RecordedValue] LIKE 'False', 0, 1) AS [Key],
+	[RecordedValue] AS [Key],
 	COUNT([TicketId]) AS [Record]
 FROM [PMS1].[dbo].[vTrackers_AllPropertiesByStatus] WITH(NOLOCK)
 WHERE [PropertyName] LIKE 'Issue CI' AND [CreatedDate] > GETDATE() - 400
