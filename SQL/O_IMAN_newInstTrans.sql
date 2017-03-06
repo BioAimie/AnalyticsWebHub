a21e9c5abac843d19b678866c451ff9980625206
bioamber <amber.kiser@biofiredx.com>
2017-03-06 13:29:56 -0700

    v2.9.0
    Updates to IMAN portfolio.

--- SQL/O_IMAN_newInstTrans.sql
+++ SQL/O_IMAN_newInstTrans.sql
@@ -1,54 +1,75 @@
 SET NOCOUNT ON
 
-SELECT *
-INTO #Serial
-FROM [SQL1-RO].[mas500_app].[dbo].[vdvSerialTransactions] WITH(NOLOCK)
-WHERE [ItemID] IN ('FLM1-ASY-0001','FLM2-ASY-0001','HTFA-ASY-0001','HTFA-ASY-0003')
-
-SELECT *
-INTO #Inven
-FROM [SQL1-RO].[mas500_app].[dbo].[vdvInventoryTran] WITH(NOLOCK)
-WHERE [ItemID] IN ('FLM1-ASY-0001','FLM2-ASY-0001','HTFA-ASY-0001','HTFA-ASY-0003')
-
-
 SELECT 
-	ROW_NUMBER() OVER (PARTITION BY [SerialNo] ORDER BY [TranDate]) AS [Row],
-	[SerialNo],
-	[TranDate],
-	s.[WhseID],
-	[TranQty]
-INTO #Rows
-FROM #Serial s LEFT JOIN #Inven i
-	ON s.[InvtTranKey] = i.[InvtTranKey]
-ORDER BY [SerialNo]
-
-SELECT 
-	[SerialNo]
+	S.[SerialNo] AS [OldSerial],
+	IIF(S.[SerialNo] LIKE 'K%R', SUBSTRING(S.[SerialNo], 2, PATINDEX('%R', S.[SerialNo])-2),
+		IIF(S.[SerialNo] LIKE '%R', SUBSTRING(S.[SerialNo], 1, PATINDEX('%R',S.[SerialNo])-1), 
+		IIF(S.[SerialNo] LIKE 'K%', SUBSTRING(S.[SerialNo], 2, LEN(S.[SerialNo])), S.[SerialNo]))) AS [SerialNo],
+	S.[ItemID],
+	S.[WhseID],
+	I.[TranDate],
+	IIF(I.[TranQty] > 0, 1,
+		IIF(I.[TranQty] < 0, -1, 0)) AS [TranQty],
+	I.[InvtTranKey]
 INTO #Serials
-FROM #Rows
-WHERE [Row] = 1 AND [WhseID] LIKE 'IFSTK'
-
-SELECT 
-	[SerialNo],
-	MIN([TranDate]) AS [Date],
-	IIF(LEFT(s.[ItemID],4) LIKE 'FLM2','FA2.0',
-		IIF(LEFT(s.[ItemID],4) LIKE 'FLM1','FA1.5',
-		IIF(s.[ItemID] LIKE 'HTFA-ASY-0001','Torch Base',
-		IIF(s.[ItemID] LIKE 'HTFA-ASY-0003', 'Torch Module',s.[ItemID])))) AS [Version]
-INTO #Stock
-FROM #Serial s LEFT JOIN #Inven i
-	ON s.[InvtTranKey] = i.[InvtTranKey]
-WHERE s.[WhseID] LIKE 'STOCK' AND [TranQty] > 0 
-	AND [SerialNo] IN (SELECT * FROM #Serials)
-GROUP BY [SerialNo], s.[ItemID]
+FROM 
+(
+	SELECT
+		[ItemID],
+		[WhseID],
+		[InvtTranKey],
+		UPPER(REPLACE(REPLACE(REPLACE(REPLACE([SerialNo],' ',''),'_',''),'-',''),'.','')) AS [SerialNo]
+	FROM [SQL1-RO].[mas500_app].[dbo].[vdvSerialTransactions] WITH(NOLOCK)
+	WHERE [ItemID] IN ('FLM1-ASY-0001','FLM2-ASY-0001','HTFA-ASY-0001','HTFA-ASY-0003') OR 
+		[ItemID] IN ('FLM1-ASY-0001R','FLM2-ASY-0001R','HTFA-ASY-0001R','HTFA-ASY-0003R')
+) S LEFT JOIN 
+(
+	SELECT
+		[TranDate],
+		[TranQty],
+		[InvtTranKey]
+	FROM [SQL1-RO].[mas500_app].[dbo].[vdvInventoryTran] WITH(NOLOCK)
+	WHERE [ItemID] IN ('FLM1-ASY-0001','FLM2-ASY-0001','HTFA-ASY-0001','HTFA-ASY-0003') OR
+		[ItemID] IN ('FLM1-ASY-0001R','FLM2-ASY-0001R','HTFA-ASY-0001R','HTFA-ASY-0003R')
+) I
+	ON S.[InvtTranKey] = I.[InvtTranKey] 
 
 SELECT
-	YEAR([Date]) AS [Year],
-	MONTH([Date]) AS [Month],
-	[SerialNo], 
-	[Version],
-	1 AS [Record]
-FROM #Stock
-ORDER BY [Year],[Month]
+	ROW_NUMBER() OVER(PARTITION BY [SerialNo] ORDER BY [TranDate], [InvtTranKey]) AS [Row],
+	[SerialNo],
+	[ItemID],
+	[TranDate]
+INTO #Rows 
+FROM #Serials 
+WHERE [WhseID] LIKE 'STOCK' AND [TranQty] > 0
+	AND [SerialNo] IN 
+(
+	SELECT 
+		[SerialNo] 
+	FROM 
+	(
+		SELECT 
+			ROW_NUMBER() OVER (PARTITION BY [SerialNo] ORDER BY [TranDate], [InvtTranKey]) AS [Row],
+			[SerialNo],
+			[TranDate],
+			[WhseID],
+			[TranQty]
+		FROM #Serials 
+	) A
+	WHERE [Row] = 1 AND [WhseID] LIKE 'IFSTK'
+)
 
-DROP TABLE #Serial, #Inven, #Stock, #Rows, #Serials
+SELECT 
+	YEAR([TranDate]) AS [Year],
+	MONTH([TranDate]) AS [Month],
+	[SerialNo],
+	IIF([ItemID] LIKE 'FLM1-%', 'FA1.5',
+		IIF([ItemID] LIKE 'FLM2-%', 'FA2.0',
+		IIF([ItemID] LIKE 'HTFA-ASY-0001%', 'Torch Base',
+		IIF([ItemID] LIKE 'HTFA-ASY-0003%', 'Torch Module', [ItemID])))) AS [Version],
+	1 AS [Record]  
+FROM #Rows
+WHERE [Row] = 1
+ORDER BY [TranDate] 
+
+DROP TABLE #Serials, #Rows
