2bd4ce9e04faa914fe3bf0b6a1c0af47b7e32fca
bioamber <amber.kiser@biofiredx.com>
2017-04-13 09:40:42 -0600

    v2.9.0
    Updates to RMA portfolio per John's request.

--- SQL/R_IRMA_PouchesShipped.sql
+++ SQL/R_IRMA_PouchesShipped.sql
@@ -1,13 +1,18 @@
 SELECT
 	YEAR([ShipDate]) AS [Year],
+	MONTH([ShipDate]) AS [Month],
 	DATEPART(ww,[ShipDate]) AS [Week],
 	'PouchesShipped' AS [Key],
+	[Panel],
 	SUM(IIF([TranType] NOT LIKE 'SH',-1*[QtyShipped],[QtyShipped])) AS [Record]
 FROM [PMS1].[dbo].[vPouchShipmentsWithAnnotations_IOID] WITH(NOLOCK)
 WHERE [ShipDate] >= (GETDATE() - 600) AND [ProductClass] LIKE 'IVD' AND [CustID] NOT LIKE 'IDATEC'
 GROUP BY 
 	YEAR([ShipDate]),
-	DATEPART(ww,[ShipDate])
+	MONTH([ShipDate]),
+	DATEPART(ww,[ShipDate]),
+	[Panel]
 ORDER BY 
 	YEAR([ShipDate]),
+	MONTH([ShipDate]),
 	DATEPART(ww,[ShipDate])
