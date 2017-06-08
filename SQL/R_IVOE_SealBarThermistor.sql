092d9b4b0c87f9934ea562960189bba98c4950cf
BioBrent <brent.kerby@biofiredx.com>
2017-06-08 12:29:02 -0600

    Minor correction to sealbar thermistor VoE - Consistently exclude converted instruments from both numerator and denominator of FA2.0 results

--- SQL/R_IVOE_SealBarThermistor.sql
+++ SQL/R_IVOE_SealBarThermistor.sql
@@ -1,14 +1,12 @@
 SET NOCOUNT ON
 
-SELECT 
+SELECT DISTINCT
+	P.[SerialNo],
+	P.[Version],
+	P.[DateOfManufacturing],
 	I.[TicketString],
-	I.[SerialNo],
-	I.[Version],
-	I.[HoursRun],
-	MIN(CAST(P.[DateOfManufacturing] AS DATE)) AS [DateOfManufacturing]
+	I.[HoursRun]
 FROM [PMS1].[dbo].[bInstrumentFailure] I
 INNER JOIN [PMS1].[dbo].[RMAServiceCodes] S ON S.[TicketId] = I.[TicketId]
 INNER JOIN [PMS1].[dbo].[bInstrumentProduced] P ON P.[NormalSerial] = I.[NormalSerial]
-WHERE TRY_CAST(S.[ServiceCode] AS INT) = 254 AND I.[CustomerId] NOT LIKE '%IDATEC%'
-GROUP BY I.[TicketString], I.[SerialNo], I.[Version], I.[HoursRun]
-ORDER BY [DateOfManufacturing]
+WHERE TRY_CAST(S.[ServiceCode] AS INT) = 254 AND I.[CustomerId] NOT LIKE '%IDATEC%' AND P.[ProdNo] = 1
